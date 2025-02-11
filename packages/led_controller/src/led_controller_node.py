5a8dd4494f59fc82d8dfacaaae300680db495153
Tina Nguyen <ngocthuy@ualberta.ca>
2025-02-11 22:02:11 +0000

    updated led color code

--- packages/led_controller/src/led_controller_node.py
+++ packages/led_controller/src/led_controller_node.py
@@ -6,6 +6,7 @@ from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import LEDPattern
 from led_controller.srv import SetLEDColor, SetLEDColorResponse
 
+# Use the HOSTNAME environment variable to build our topic name.
 host_name = os.environ["HOSTNAME"]
 
 def get_color_rgba(color):
@@ -23,27 +24,33 @@ def get_color_rgba(color):
     elif color == "off":
         return ColorRGBA(r=0.0, g=0.0, b=0.0, a=0.0)
     else:
-        # Default to white if color not recognized.
+        # Default to white if the color is not recognized.
         return ColorRGBA(r=1.0, g=1.0, b=1.0, a=1.0)
 
 class LEDController(DTROS):
     def __init__(self, node_name):
+        """
+        Initialize the LED Controller node.
+        """
         # DTROS automatically initializes the node.
         super(LEDController, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-        
+
+        # Publisher for LED patterns. Adjust the topic name as needed.
         publisher_topic = f"/{host_name}/led_emitter_node/led_pattern"
         self.led_pub = rospy.Publisher(publisher_topic, LEDPattern, queue_size=10)
-        
-        self.current_color = "blue"  # default color
-        
+
+        # Default color is blue.
+        self.current_color = "blue"
+
+        # Service to change the LED color.
         self.led_service = rospy.Service("/led/set_color", SetLEDColor, self.handle_set_color)
-        
+
         rospy.loginfo("LED Controller Node is up and running, current color: %s", self.current_color)
         rospy.on_shutdown(self.shutdown_hook)
 
     def handle_set_color(self, req):
         """
-        Service callback that changes the LED color.
+        Service callback to change the LED color.
         """
         rospy.loginfo("LED Controller: Changing LED color to: %s", req.color)
         self.current_color = req.color
@@ -54,34 +61,44 @@ class LEDController(DTROS):
     def run(self):
         """
         Publish the current LED pattern at a fixed rate.
+        In this example, we assume that the LED emitter expects a pattern for 5 LEDs.
         """
         rate = rospy.Rate(10)  # 10 Hz
         while not rospy.is_shutdown():
             led_msg = LEDPattern()
             # Set the header with the current timestamp.
             led_msg.header = Header(stamp=rospy.Time.now())
-            # Populate both color_list and rgb_vals fields.
-            led_msg.color_list = [self.current_color]
-            led_msg.rgb_vals = [get_color_rgba(self.current_color)]
-            # For a steady light, set frequency to 0.0.
-            led_msg.frequency = 0.0
+            # Populate the LED pattern lists with 5 identical entries.
+            led_msg.color_list = [self.current_color] * 5
+            led_msg.rgb_vals = [get_color_rgba(self.current_color)] * 5
+            # Optionally, set masks if your emitter node uses them.
+            led_msg.color_mask = [1] * 5
+            led_msg.frequency = 0.0  # steady (non-blinking) light
+            led_msg.frequency_mask = [0] * 5
             self.led_pub.publish(led_msg)
             rate.sleep()
 
     def shutdown_hook(self):
         """
-        Publish an 'off' command when the node shuts down.
+        Called when the node shuts down.
+        Turns off the LED by publishing an 'off' pattern.
         """
         rospy.loginfo("Shutting down LED Controller, turning off LED.")
         led_msg = LEDPattern()
         led_msg.header = Header(stamp=rospy.Time.now())
-        led_msg.color_list = ["off"]
-        led_msg.rgb_vals = [get_color_rgba("off")]
+        # Send an "off" command to all LED positions.
+        led_msg.color_list = ["off"] * 5
+        led_msg.rgb_vals = [get_color_rgba("off")] * 5
         led_msg.frequency = 0.0
+        led_msg.frequency_mask = [0] * 5
+        led_msg.color_mask = [1] * 5
         self.led_pub.publish(led_msg)
         rospy.sleep(1)  # Allow time for the message to be sent.
 
 if __name__ == '__main__':
-    # DTROS handles node initialization.
+    # Initialize the LED controller node.
     node = LEDController("led_controller_node")
-    rospy.spin()
+    try:
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
